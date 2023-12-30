3d665b498728e3662ef683b0fdcb93ab021b2194
VictorGalkevich <2vektor54@gmail.com>
2023-12-30 23:05:14 +0300

    config + tests + formatters

--- src/main/java/by/itstep/springApplication/dto/UserCreateEditDto.java
+++ src/main/java/by/itstep/springApplication/dto/UserCreateEditDto.java
@@ -2,12 +2,16 @@ package by.itstep.springApplication.dto;
 
 import by.itstep.springApplication.database.entity.Role;
 import lombok.Value;
+import lombok.experimental.FieldNameConstants;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @Value
+@FieldNameConstants
 public class UserCreateEditDto {
     String username;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     LocalDate birthDate;
     String firstname;
     String lastname;
